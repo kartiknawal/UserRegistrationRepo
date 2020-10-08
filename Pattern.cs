f1c034c1a0fac150d0b4639a76b2cf71752d7111
kartiknawal <kartiknawal.7@gmail.com>
2020-10-08 08:24:41 +0530

    [kartiknawal] add. validate email id

--- Pattern.cs
+++ Pattern.cs
@@ -8,6 +8,7 @@ namespace UserRegistrationRegex
     class Pattern
     {
         string Name = "^[A-Z][a-z0-9A-Z]{3,}";
+        string EmailId = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2,})?$";
         public void validateName(string name)
         {
             if (Regex.IsMatch(name, Name))
@@ -17,6 +18,17 @@ namespace UserRegistrationRegex
             else
             {
                 Console.WriteLine("Invalid name");
+            }
+        }
+        public void validateEmail(string email)
+        {
+            if (Regex.IsMatch(email, EmailId))
+            {
+                Console.WriteLine("Valid email");
+            }
+            else
+            {
+                Console.WriteLine("invalid email");
             }
         }
     }
