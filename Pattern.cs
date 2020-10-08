a81fea2280c96e1d49f91a5c1264cea0a920ecf5
kartiknawal <kartiknawal.7@gmail.com>
2020-10-08 07:59:03 +0530

    [kartiknawal] add. validate first name

---
+++ Pattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    class Pattern
+    {
+        string firstName = "^[A-Z][a-z0-9A-Z]{3,}";
+        public void validateFirstName(string fname)
+        {
+            if (Regex.IsMatch(fname, firstName))
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace UserRegistrationRegex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the User Registration Problem");
+            Console.WriteLine("Welcome to the Regex User Registration Problem");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter the first name");
+            string fname = Console.ReadLine();
+            pattern.validateFirstName(fname);
         }
     }
 }
